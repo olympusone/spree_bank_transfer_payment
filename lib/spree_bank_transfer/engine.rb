fb1fe71b9c05bbd47c6a3a5dc2ea66ac4332a983
dimidev <dimidev@olympusone.com>
2021-08-17 00:48:44 +0300

    up

--- lib/spree_bank_transfer/engine.rb
+++ lib/spree_bank_transfer/engine.rb
@@ -4,20 +4,55 @@ module SpreeBankTransfer
     isolate_namespace Spree
     engine_name 'spree_bank_transfer'
 
+    # config.autoload_paths += %W(#{config.root}/lib)
+
+    initializer 'spree.register.payment_methods', after: :after_initialize do |_app|
+      _app.config.spree.payment_methods << Spree::PaymentMethod::BankTransfer
+    end
+
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
     end
 
-    initializer 'spree_bank_transfer.environment', before: :load_config_initializers do |_app|
-      SpreeBankTransfer::Config = SpreeBankTransfer::Configuration.new
-    end
+    # initializer 'spree_bank_transfer.environment', before: :load_config_initializers do |_app|
+    #   SpreeBankTransfer::Config = SpreeBankTransfer::Configuration.new
+    # end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/spree/*_decorator*.rb')) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+
       end
+      Dir.glob(File.join(File.dirname(__FILE__), '../../lib/active_merchant/**/*_decorator*.rb')) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
+
+    #   if self.frontend_available?
+    #     Dir.glob(File.join(File.dirname(__FILE__), '../../lib/spree_frontend/controllers/spree/*_decorator*.rb')) do |c|
+    #       Rails.application.config.cache_classes ? require(c) : load(c)
+    #     end
+    #   end
     end
+
+    # def self.backend_available?
+    #   @@backend_available ||= ::Rails::Engine.subclasses.map(&:instance).map{ |e| e.class.to_s }.include?('Spree::Backend::Engine')
+    # end
+
+    # def self.frontend_available?
+    #   @@frontend_available ||= ::Rails::Engine.subclasses.map(&:instance).map{ |e| e.class.to_s }.include?('Spree::Frontend::Engine')
+    # end
+
+    # if self.backend_available?
+    #   paths["app/views"] << "lib/views/backend"
+    # end
+
+    # paths['app/controllers'] << 'lib/controllers'
+
+    # if self.frontend_available?
+    #   paths["app/controllers"] << "lib/spree_frontend/controllers"
+    #   paths["app/views"] << "lib/views/frontend"
+    # end
 
     config.to_prepare(&method(:activate).to_proc)
   end
--- spree_bank_transfer.gemspec
+++ spree_bank_transfer.gemspec
@@ -14,7 +14,7 @@ Gem::Specification.new do |s|
 
   s.author    = 'You'
   s.email     = 'you@example.com'
-  s.homepage  = 'https://github.com/your-github-handle/spree_bank_transfer'
+  s.homepage  = 'https://github.com/olympusone/spree_bank_transfer'
   s.license = 'BSD-3-Clause'
 
   s.files       = `git ls-files`.split("\n").reject { |f| f.match(/^spec/) && !f.match(/^spec\/fixtures/) }
