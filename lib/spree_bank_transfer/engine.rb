49b1576f216f41dea1b85319008f812225220280
dimidev <dimidev@olympusone.com>
2022-07-19 13:28:29 +0300

    up

--- lib/spree_bank_transfer/engine.rb
+++ lib/spree_bank_transfer/engine.rb
@@ -4,15 +4,13 @@ module SpreeBankTransfer
     isolate_namespace Spree
     engine_name 'spree_bank_transfer'
 
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    initializer 'spree.register.payment_methods', after: :after_initialize do |_app|
-      _app.config.spree.payment_methods << Spree::PaymentMethod::BankTransfer
-    end
-
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
+    end
+
+    initializer 'spree.register.payment_methods', after: :after_initialize do |_app|
+      _app.config.spree.payment_methods << Spree::PaymentMethod::BankTransfer
     end
 
     initializer 'spree_bank_transfer.environment', before: :load_config_initializers do |_app|
@@ -20,12 +18,8 @@ module SpreeBankTransfer
     end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/spree/*_decorator*.rb')) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-
-      end
-      Dir.glob(File.join(File.dirname(__FILE__), '../../lib/active_merchant/**/*_decorator*.rb')) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
 
